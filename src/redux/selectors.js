7e59aadd05eda8f2ecdf86020488c267bf4e2c04
Geljinskiy <savvageljinskiy@gmail.com>
2023-02-24 02:10:01 -0500

    added memoization

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 // atomic selectors
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
@@ -5,18 +7,20 @@ export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
 
 // composite selectors
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state);
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    console.log('calcuating');
 
-  const regNormolize = sentence => {
-    return sentence.toLowerCase().trim();
-  };
+    const regNormolize = sentence => {
+      return sentence.toLowerCase().trim();
+    };
 
-  return contacts.filter(contact => {
-    return (
-      regNormolize(contact.name).includes(regNormolize(filter)) ||
-      regNormolize(contact.phone).includes(regNormolize(filter))
-    );
-  });
-};
+    return contacts.filter(contact => {
+      return (
+        regNormolize(contact.name).includes(regNormolize(filter)) ||
+        regNormolize(contact.phone).includes(regNormolize(filter))
+      );
+    });
+  }
+);
