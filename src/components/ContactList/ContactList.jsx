37c0a0f5d00ae21d5150c8bebc25a0305b6b208f
Geljinskiy <savvageljinskiy@gmail.com>
2023-02-24 00:45:46 -0500

    added composite selector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,11 @@
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 
 import css from './ContactList.module.css';
 import ContactItem from 'components/ContactItem';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-
-  const getVisibleContacts = () => {
-    const regNormolize = sentence => {
-      return sentence.toLowerCase().trim();
-    };
-    return contacts.filter(contact => {
-      return (
-        regNormolize(contact.name).includes(regNormolize(filter)) ||
-        regNormolize(contact.phone).includes(regNormolize(filter))
-      );
-    });
-  };
-
-  const visibleContacts = getVisibleContacts();
+  const visibleContacts = useSelector(selectVisibleContacts);
 
   return (
     <ul>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,22 @@
+// atomic selectors
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
+
+// composite selectors
+export const selectVisibleContacts = state => {
+  const contacts = selectContacts(state);
+  const filter = selectFilter(state);
+
+  const regNormolize = sentence => {
+    return sentence.toLowerCase().trim();
+  };
+
+  return contacts.filter(contact => {
+    return (
+      regNormolize(contact.name).includes(regNormolize(filter)) ||
+      regNormolize(contact.phone).includes(regNormolize(filter))
+    );
+  });
+};
